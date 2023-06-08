270a9f880114134ec71663f362e151bfa1f2ef1d
Defitra <deff@devs.com>
2023-06-08 13:34:37 +0700

    fixing missing key

--- components/cardList.js
+++ components/cardList.js
@@ -5,7 +5,7 @@ function CardList() {
   return (
     <div className="grid grid-col-1 md:grid-cols-4 md:grid-row-2 gap-x-[20px] gap-y-[30px] mb-10">
       {PRODUCTS.map((item) => (
-        <Card item={item}></Card>
+        <Card key={item.name} item={item}></Card>
       ))}
     </div>
   );
